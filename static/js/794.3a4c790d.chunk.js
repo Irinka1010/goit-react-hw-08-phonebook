34467fe4c1ac0d66d2d84ed7a8b6ac6842db237a
Irinka1010 <97122174+Irinka1010@users.noreply.github.com>
2022-11-08 22:55:04 +0000

    Deploying to gh-pages from @ Irinka1010/goit-react-hw-08-phonebook@07070e349254a5fb654fb54add0518a4d6df22dd 🚀

--- static/js/794.3a4c790d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{1794:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(1087),o=n(184);function c(){return(0,o.jsxs)("div",{children:[(0,o.jsx)("h1",{children:"Page not found"}),(0,o.jsx)(r.rU,{to:"/",children:"To home page"})]})}}}]);
-//# sourceMappingURL=794.3a4c790d.chunk.js.map
