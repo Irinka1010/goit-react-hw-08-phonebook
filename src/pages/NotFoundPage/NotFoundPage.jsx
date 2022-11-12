fe76024e4223812bd13053aef57458ebd35051de
dimon4uk1990 <mexal07@ukr.net>
2022-11-12 09:48:17 +0200

    fix bug img

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,17 +1,10 @@
 import { Link } from 'react-router-dom';
 import Container from '@mui/material/Container';
-import CardMedia from '@mui/material/CardMedia';
-import Imge from '../../imges/picture.png';
 
 export default function NotFoundPage() {
   return (
     <Container sx={{ mt: '1rem' }}>
-      <CardMedia
-        component="img"
-        sx={{ height: '100%' }}
-        image={Imge}
-        alt="phonebook.jpg"
-      />
+      <h1>Project your phone book</h1>
       <Link to="/"></Link>
     </Container>
   );
